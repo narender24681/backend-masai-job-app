f7c1571ecf528836892baf1086c3c4a4b8ffa783
Narender <narender24681@gmail.com>
2023-06-13 13:30:15 +0530

    modified backend

--- models/Jobs.model.js
+++ models/Jobs.model.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 
 const jobsSchema = mongoose.Schema({
     company: {type: String, required: true},
+    postedAt: {type: String, required: true},
     city: {type: String, required: true},
     location: {type: String, required: true},
     role: {type: String, required: true},
     level: {type: String, required: true},
+    contract: {type: String, required: true},
     position: {type: String, required: true},
     language: {type: String, required: true},
-    contract: {type: String, required: true}
 });
 
 const JobsModel = mongoose.model("jobs", jobsSchema);
